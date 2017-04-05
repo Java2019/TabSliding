d75fa51cb0ec856e2800d8f0045d651cd4cfa421
java2019 <java_2019@mail.ru>
2017-04-05 11:04:01 +0300

    test 1

---
+++ app/src/main/java/com/example/use/tabsliding/GamesFragment.java
@@ -0,0 +1,22 @@
+package com.example.use.tabsliding;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by use on 05.04.17.
+ */
+public class GamesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_games, container, false);
+
+        return rootView;
+    }
+}
